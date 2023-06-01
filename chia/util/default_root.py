e6a2fd84364c10c57a6d7f6aeafb190d77202fe5
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-06-01 11:06:37 +0200

    update 5

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -6,3 +6,7 @@ from pathlib import Path
 DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CRYPTOMINES_ROOT", "~/.cryptomines/mainnet"))).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CRYPTOMINES_KEYS_ROOT", "~/.cryptomines_keys"))).resolve()
+
+SIMULATOR_ROOT_PATH = Path(
+    os.path.expanduser(os.getenv("CRYPTOMINES_SIMULATOR_ROOT", f"{DEFAULT_ROOT_PATH.parent}/simulator"))
+).resolve()
--- chia/util/service_groups.py
+++ chia/util/service_groups.py
@@ -3,27 +3,33 @@ from __future__ import annotations
 from typing import Generator, KeysView
 
 SERVICES_FOR_GROUP = {
-    "all": (
-        "cryptomines_harvester cryptomines_timelord_launcher cryptomines_timelord cryptomines_farmer "
-        "cryptomines_full_node cryptomines_wallet cryptomines_data_layer cryptomines_data_layer_http"
-    ).split(),
+    "all": [
+        "cryptomines_harvester",
+        "cryptomines_timelord_launcher",
+        "cryptomines_timelord",
+        "cryptomines_farmer",
+        "cryptomines_full_node",
+        "cryptomines_wallet",
+        "cryptomines_data_layer",
+        "cryptomines_data_layer_http",
+    ],
     # TODO: should this be `data_layer`?
-    "data": "cryptomines_wallet cryptomines_data_layer".split(),
-    "data_layer_http": "cryptomines_data_layer_http".split(),
-    "node": "cryptomines_full_node".split(),
-    "harvester": "cryptomines_harvester".split(),
-    "farmer": "cryptomines_harvester cryptomines_farmer cryptomines_full_node cryptomines_wallet".split(),
-    "farmer-no-wallet": "cryptomines_harvester cryptomines_farmer cryptomines_full_node".split(),
-    "farmer-only": "cryptomines_farmer".split(),
-    "timelord": "cryptomines_timelord_launcher cryptomines_timelord cryptomines_full_node".split(),
-    "timelord-only": "cryptomines_timelord".split(),
-    "timelord-launcher-only": "cryptomines_timelord_launcher".split(),
-    "wallet": "cryptomines_wallet".split(),
-    "introducer": "cryptomines_introducer".split(),
-    "simulator": "cryptomines_full_node_simulator".split(),
-    "crawler": "cryptomines_crawler".split(),
-    "seeder": "cryptomines_crawler cryptomines_seeder".split(),
-    "seeder-only": "cryptomines_seeder".split(),
+    "data": ["cryptomines_wallet", "cryptomines_data_layer"],
+    "data_layer_http": ["cryptomines_data_layer_http"],
+    "node": ["cryptomines_full_node"],
+    "harvester": ["cryptomines_harvester"],
+    "farmer": ["cryptomines_harvester", "cryptomines_farmer", "cryptomines_full_node", "cryptomines_wallet"],
+    "farmer-no-wallet": ["cryptomines_harvester", "cryptomines_farmer", "cryptomines_full_node"],
+    "farmer-only": ["cryptomines_farmer"],
+    "timelord": ["cryptomines_timelord_launcher", "cryptomines_timelord", "cryptomines_full_node"],
+    "timelord-only": ["cryptomines_timelord"],
+    "timelord-launcher-only": ["cryptomines_timelord_launcher"],
+    "wallet": ["cryptomines_wallet"],
+    "introducer": ["cryptomines_introducer"],
+    "simulator": ["cryptomines_full_node_simulator"],
+    "crawler": ["cryptomines_crawler"],
+    "seeder": ["cryptomines_crawler", "cryptomines_seeder"],
+    "seeder-only": ["cryptomines_seeder"],
 }
 
 
